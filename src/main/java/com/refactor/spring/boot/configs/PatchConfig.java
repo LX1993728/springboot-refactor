4cb9cf023757e64e1853203be56f1e4e7e6c77c2
LX1993728 <2652790899@qq.com>
2020-08-11 15:57:17 +0800

    为Springboot填坑

---
+++ src/main/java/com/refactor/spring/boot/configs/PatchConfig.java
@@ -0,0 +1,17 @@
+package com.refactor.spring.boot.configs;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @author liuxun
+ * @apiNote 此配置不建议使用，但对于一些老项目(使用了很多原生SpringAOP)如果缺少此类的配置会不兼容。
+ * 注意：只要配置了WebMvcConfigurationSupport，自动配置就会失效，必须使用@EnableWebMvc来接管自动配置
+ */
+@Configuration
+@EnableWebMvc
+public class PatchConfig extends WebMvcConfigurationSupport {
+
+}
