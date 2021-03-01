4da1f23a4ed1f180a5f64bf2b101779e2036ac8b
liuxun  <liuxun@cyou-inc.com>
2021-03-01 15:38:27 +0800

    ++++ 添加日志打印 +++

--- src/main/java/com/refactor/spring/boot/controllers/TestController2.java
+++ src/main/java/com/refactor/spring/boot/controllers/TestController2.java
@@ -1,14 +1,17 @@
 package com.refactor.spring.boot.controllers;
 
+import com.refactor.spring.boot.domains.JSONResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
+@Controller
 public class TestController2 {
 
     @Autowired
@@ -19,10 +22,17 @@ public class TestController2 {
 
     // 测试
     @GetMapping("/testGetProfile")
+    @ResponseBody
     public Object getProfile(){
         for (String profile : environment.getActiveProfiles()){
             log.info("-------- " + profile);
         }
         return  profile;
+    }
+
+    @GetMapping("/testHttpLog")
+    @ResponseBody
+    public Object getHttpLog(){
+      return   new JSONResult("00000","成功");
     }
 }
---
+++ src/main/java/com/refactor/spring/boot/domains/JSONResult.java
@@ -0,0 +1,67 @@
+package com.refactor.spring.boot.domains;
+
+import java.io.StringWriter;
+import java.io.Writer;
+
+public class JSONResult {
+
+	private String code;
+	private String msg;
+	private Object obj;
+	private Integer count;// add by CT 2015-10-26 分页处理的总行数
+
+	public JSONResult() {
+	}
+
+	public JSONResult(String code, String msg) {
+		this.code = code;
+		this.msg = msg;
+		this.obj = "";
+	}
+
+	public JSONResult(String code, String msg, Object obj) {
+		this.code = code;
+		this.msg = msg;
+		this.obj = obj;
+	}
+
+	public JSONResult(String code, String msg, Object obj, Integer count) {
+		this.code = code;
+		this.msg = msg;
+		this.obj = obj;
+		this.count = count;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+}
